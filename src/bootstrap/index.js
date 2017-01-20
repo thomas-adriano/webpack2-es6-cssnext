4b48505df0f71387e982839fd46586b7fc7c12eb
thomas.adriano <thomas.adriano@senior.com.br>
2017-01-20 15:49:58 -0200

    CSS-modules configuration removed, css resets and color variables added

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,6 +1,3 @@
 import 'bootstrap/index.html';
 
-import '../../assets/Nunito_Sans';
-
-
 console.log("HHHEEELLLOOO!!!!");
--- webpack/webpack.rules.js
+++ webpack/webpack.rules.js
@@ -1,34 +1,42 @@
 module.exports = [{
-        test: /\.css$/,
-        use: [
-            // "style-loader",
-            "file-loader",
-            "extract-loader",
-            {
-                loader: 'css-loader',
-                options: {
-                    importLoaders: 1,
-                    modules: true,
-                    localIdentName: '[name]__[local]___[hash:base64:5]'
-                }
-            },
-            'postcss-loader'
-        ]
-    },
-    { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader' },
-    {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: 'babel-loader',
-    },
-    {
-        test: /\.html$/,
-        use: [{
-            loader: 'html-loader',
+    test: /\.css$/,
+    use: [
+        // "style-loader",
+        "file-loader",
+        "extract-loader",
+        {
+            loader: 'css-loader',
             options: {
-                interpolate: true,
-                attrs: ["link:href"]
+                importLoaders: 1,
             }
-        }, ]
+        },
+        'postcss-loader'
+    ]
+},
+{
+    test: /\.(woff|woff2|eot|ttf|svg)$/, use: 'url-loader'
+},
+{
+    test: /\.(jpg|jpeg|gif|png)$/, use: {
+        loader: 'url-loader',
+        options: {
+            limit: 2000
+        }
     }
+},
+{
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: 'babel-loader',
+},
+{
+    test: /\.html$/,
+    use: [{
+        loader: 'html-loader',
+        options: {
+            interpolate: true,
+            attrs: ["link:href"]
+        }
+    },]
+}
 ]
